ba5ca8569a34e3a7a2642858d8b7c8bed3826072
AndreaLavezzi <andrea.lavezzi@iisviolamarchesini.edu.it>
2021-11-15 19:04:15 +0100

    Terminata documentazione funzioni relative alla gestione delle partite, inizio documentazione funzioni di gestione dell'asta

--- search/functions_1.js
+++ search/functions_1.js
@@ -6,9 +6,9 @@ var searchData=
   ['caricapartita_3',['CaricaPartita',['../classfantacalcio_1_1_program.html#a93d92ed927aa9e4d710f6bd7de0beee6',1,'fantacalcio::Program']]],
   ['classifica_4',['Classifica',['../classfantacalcio_1_1_program.html#a1443cfac714b65c4060c4aeafc3798a3',1,'fantacalcio::Program']]],
   ['compareto_5',['CompareTo',['../classfantacalcio_1_1_giocatore.html#aa8a7022e5e49531f3ecc39b036ba50cb',1,'fantacalcio::Giocatore']]],
-  ['controlloasta_6',['ControlloAsta',['../classfantacalcio_1_1_program.html#aa67eb15a61da310a10f273f9abca46c2',1,'fantacalcio::Program']]],
+  ['controlloasta_6',['ControlloAsta',['../classfantacalcio_1_1_program.html#a92f4835e85c4527993de50263a2213a0',1,'fantacalcio::Program']]],
   ['controllonome_7',['ControlloNome',['../classfantacalcio_1_1_program.html#a7e5203f16d196ade698ae87c6de31703',1,'fantacalcio::Program']]],
   ['controlloruolo_8',['ControlloRuolo',['../classfantacalcio_1_1_program.html#a4301631bfd31d1a56d709aabf6afe92e',1,'fantacalcio::Program']]],
-  ['creagiocatori_9',['CreaGiocatori',['../classfantacalcio_1_1_program.html#accdc0986fc5cf2db5b4410b69a09bccf',1,'fantacalcio::Program']]],
+  ['creagiocatori_9',['CreaGiocatori',['../classfantacalcio_1_1_program.html#a3a7c02786acc6c97956d51b7b921513f',1,'fantacalcio::Program']]],
   ['creasalvataggio_10',['CreaSalvataggio',['../classfantacalcio_1_1_salvataggio.html#a6128a937f04c57969d568e542415efb8',1,'fantacalcio::Salvataggio']]]
 ];
