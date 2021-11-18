de1a43e819d41e3142642c703738769f850bd4b8
AndreaLavezzi <andrea.lavezzi@iisviolamarchesini.edu.it>
2021-11-18 22:49:35 +0100

    Aggiornata documentazione

--- search/all_a.js
+++ search/all_a.js
@@ -3,7 +3,7 @@ var searchData=
   ['partita_0',['Partita',['../classfantacalcio_1_1_program.html#a20f3aee584244afc790ea2099b0348d3',1,'fantacalcio::Program']]],
   ['partitaincorso_1',['partitaInCorso',['../classfantacalcio_1_1_program.html#afd9cd99e730ed8102fa7b8e083a143f2',1,'fantacalcio::Program']]],
   ['peso_2',['peso',['../classfantacalcio_1_1_azione.html#a5f88df0a75985f8f0745c5fc85677dec',1,'fantacalcio::Azione']]],
-  ['prepartita_3',['PrePartita',['../classfantacalcio_1_1_program.html#a67687dd256a8e036908f1cf792693713',1,'fantacalcio::Program']]],
+  ['prepartita_3',['PrePartita',['../classfantacalcio_1_1_program.html#a7dfc277488ec1d749c73f10479237104',1,'fantacalcio::Program']]],
   ['primapartita_4',['primaPartita',['../classfantacalcio_1_1_giocatore.html#a54a29ab3ede9229d3c4efbd8ee586c77',1,'fantacalcio::Giocatore']]],
   ['program_5',['Program',['../classfantacalcio_1_1_program.html',1,'fantacalcio']]],
   ['program_2ecs_6',['Program.cs',['../_program_8cs.html',1,'']]],
